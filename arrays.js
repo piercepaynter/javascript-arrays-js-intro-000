528edab678a646df9dcf5a49823e7303f1ac96fa
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-19 04:36:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)]
+  return array = [element, array]
+  //return array.unshift(element)]
 }
 
 function addElementToEndOfArray(array, element) {
