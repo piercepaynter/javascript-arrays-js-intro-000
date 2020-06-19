98e8482c7d739324a9b7211db7d59a412657df06
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-19 04:11:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(array, element){
-  return array.unshift(element);
+function addElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
 }
 
 //function destructivelyAddElementToBeginningOfArray{
