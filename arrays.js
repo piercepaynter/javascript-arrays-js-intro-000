39ce8b5525fbbdceb7ad3f20f8c30439b3c67193
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-19 04:41:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,16 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   //return array = [element, array]
-  return array.unshift(element)]
+  return array.unshift(element)];
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element]
+  return [...array, element];
 }
 
 //function destructivelyAddElementToEndOfArray(array, element) {
