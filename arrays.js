53b6f27de044a3ea0ccd51d95baaa8477925a6b3
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-19 03:55:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild Cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
-
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(){
+  
+}
+function destructivelyAddElementToBeginningOfArray{
+  
+}
